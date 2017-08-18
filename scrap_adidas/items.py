2bae5fa465016a898ed9db80bfd5842b6adc403d
Waleed-Ahmed94 <chwaleedjajja@gmail.com>
2017-08-18 16:18:08 +0500

    Add files via upload

---
+++ scrap_adidas/items.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ProductItem(scrapy.Item):
+    
+    product_url = scrapy.Field()
+    store_keeping_unit = scrapy.Field()
+    title = scrapy.Field()
+    brand = scrapy.Field()
+    description = scrapy.Field()
+    price = scrapy.Field()
+    currency = scrapy.Field()
+    variations = scrapy.Field()
+
+
+class VariationItem(scrapy.Item):
+
+    display_color_name = scrapy.Field()
+    image_urls = scrapy.Field()
+    size = scrapy.Field()
+
+class SizeItem(scrapy.Item):
+
+    size_name = scrapy.Field()
+    size_quantity = scrapy.Field()
